e618aedc457c65910aaec5ffcccf12a81011ef42
Antonio Casero Palmero <antonio.palmero@here.com>
2020-02-05 22:28:45 +0100

    Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Panels",
+            dependencies: [],
             path: "Sources",
-            dependencies: []
         ),
         .testTarget(
             name: "PanelsTests",
